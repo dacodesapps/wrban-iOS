14697c38a5c6ce06a3450b37f1b19f9cb420744a
Dacodes <dacodes@imac-de-dacodes.local>
2015-12-14 17:05:02 -0600

    Initial Commit

---
+++ Wrban/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wrban
+//
+//  Created by Dacodes on 14/12/15.
+//  Copyright © 2015 Dacodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wrban/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wrban
+//
+//  Created by Dacodes on 14/12/15.
+//  Copyright © 2015 Dacodes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
